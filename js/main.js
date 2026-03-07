eb6975159bdca861e8b8ac66b03b4962cfa3202e
Nguyễn Hoàng <minhhoangprovjp123@gmail.com>
2026-03-07 22:58:03 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Main JavaScript File
-// Khởi tạo ứng dụng tại đây
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Application initialized');
-  // Thêm code khởi tạo ứng dụng
-});
