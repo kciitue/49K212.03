4518cb0967f5087cf27bbc7765be99ce2bece72e
Huy Bui <huybuixuan248@gmail.com>
2026-03-15 00:13:09 +0700

    Scrum spint 2 finished

--- Group03_project/quanlytro/quanlytro/settings.py
+++ Group03_project/quanlytro/quanlytro/settings.py
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Ho_Chi_Minh'
+LANGUAGE_CODE = 'vi'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
 USE_I18N = True
 USE_L10N = True
